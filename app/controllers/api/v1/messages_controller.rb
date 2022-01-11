87db22c98cf38007a5ac5d90bf13d0b4c4e8dbb4
Juliana Ifionu <julieifionu@gmail.com>
2022-01-11 21:38:39 +0100

    Fix API cors errors

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,8 +2,8 @@ module Api
   module V1
     class MessagesController < ApplicationController
       def index
-        @message = Message.all.order('random()').limit(1)
-        render json: { greetings: @message }.to_json
+        @greetings = Message.all.order('random()').limit(1)
+        render json: @greetings
       end
     end
   end
