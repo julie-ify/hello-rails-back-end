7fdc8d313bd2c0ecaa06f6f9e0ec4e8185311bc7
Juliana Ifionu <julieifionu@gmail.com>
2022-01-11 11:51:59 +0100

    Render random messages from databse in json format

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,10 @@
 module Api
   module V1
     class MessagesController < ApplicationController
-      
+      def index
+        @message = Message.all.order('random()').limit(1)
+        render json: { greetings: @message }.to_json
+      end
     end
   end
 end
